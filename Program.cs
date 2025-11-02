b4becc9a4c737c3480baa8e3f0044922fe953694
Danielly Oliveira <danyoliveiraa001@gmail.com>
2025-11-02 16:48:43 -0300

    Conclusão

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Aprendendo a usar strings
+
+string cidade = "Itanhaém";
+string estado = "SP";
+string cidadeComEstado = $"Minha escola fica em {cidade} / {estado.ToUpper()}.";
+
+Console.WriteLine(cidadeComEstado);
+
+// \n quebra de linha
+// @ desliga os comandos "ocultos"
+// \\ indica que voce quer usar uma unica barra
+// \"\" mostra aspas dentro da string 
+// $ template de string
+// f2 modifica o nome de alguma variável
